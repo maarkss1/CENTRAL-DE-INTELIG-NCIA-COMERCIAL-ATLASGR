07bb30bb89f1d0f41d9193fa1cb9500868d334ae
MarksN <marcelinmark@gmail.com>
2026-08-06 00:10:33 -0300

    feat: change frontend port to 3005

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(() => {
     },
     server: {
       host: true, // Permite acesso via rede (0.0.0.0)
+      port: 3005,
       proxy: {
         '/api': {
           target: 'http://localhost:3000',
